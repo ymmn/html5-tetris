ba0bdfc8721e06b71cc46ed0ac8e22ebd78f512e
ymmn <abdnimery92@gmail.com>
2013-12-25 18:25:17 -0600

    adjusting speed

--- src/Player.js
+++ src/Player.js
@@ -1,14 +1,42 @@
 function Player (board, controls) {
 	
 	var _bounce = {};
+	var _firstStep = {
+		right: 7,
+		left: 7
+	};
 	var _rate = {
-		right: 5,
-		left: 5,
+		right: 3,
+		left: 3,
 		up: 30,
 		space: 30,
 		pause: 30,
 		down: 4,
 		swap: 30
+	};
+
+	var _debounceLeftRight = function(input, key) {
+		/* not pressed at all */
+		if( !input[key] ) {
+			_bounce[key] = undefined;
+			return false;
+		}
+
+		var curTicks = createjs.Ticker.getTicks();
+		var firstPressed = _bounce[key];
+		if( firstPressed === undefined ) {
+			_bounce[key] = curTicks;
+			return true;
+		} else {
+			var d = curTicks - firstPressed;
+			/* is it first step? */
+			if( d < _firstStep[key] ) {
+				return false;
+			} else {
+				d -= _firstStep[key];
+				return d % _rate[key] === 0;
+			}
+		}
 	};
 
 	var _debounce = function(input, key) {
@@ -19,20 +47,21 @@ function Player (board, controls) {
 		}
 
 		var curTicks = createjs.Ticker.getTicks();
-		var lastUsed = _bounce[key];
-		if( lastUsed === undefined || curTicks - lastUsed > _rate[key] ) {
+		var firstPressed = _bounce[key];
+
+		if( firstPressed === undefined || (curTicks - firstPressed) % _rate[key] === 0 ) {
 			_bounce[key] = curTicks;
 			return true;
 		}
 		return false;
-	};
+	}
 
 	this.tick = function(input){
 		var ret = false;
-		if( _debounce(input, "left") ) {
+		if( _debounceLeftRight(input, "left") ) {
 			board.termino.move(-1, 0);
 		}
-		if( _debounce(input, "right") ) {
+		if( _debounceLeftRight(input, "right") ) {
 			board.termino.move(1, 0);
 		}
 		if( _debounce(input, "down") ) {

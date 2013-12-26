41e25bc960b9dd10a52acc428e64846bb65edb57
ymmn <abdnimery92@gmail.com>
2013-12-25 18:10:20 -0600

    adjusting speed

--- src/Player.js
+++ src/Player.js
@@ -4,16 +4,19 @@ function Player (board, controls) {
 	var _rate = {
 		right: 5,
 		left: 5,
-		up: 10,
-		space: 10,
-		pause: 10,
+		up: 30,
+		space: 30,
+		pause: 30,
 		down: 4,
-		swap: 10
+		swap: 30
 	};
 
 	var _debounce = function(input, key) {
 		/* not pressed at all */
-		if( !input[key] ) return false;
+		if( !input[key] ) {
+			_bounce[key] = undefined;
+			return false;
+		}
 
 		var curTicks = createjs.Ticker.getTicks();
 		var lastUsed = _bounce[key];
